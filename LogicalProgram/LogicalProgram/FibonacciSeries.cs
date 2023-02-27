2a850dc1bf0240b5b20248e3f1f9a5fef3f8d7fb
unknown <bhotnikhil25@gmail.com>
2023-02-27 09:27:40 +0530

    [Nikhil]-Fibonacci Series

---
+++ LogicalProgram/LogicalProgram/FibonacciSeries.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class FibonacciSeries
+    {
+        public static void Fibonacci_Recurrsion(int len)
+        {
+            int previousToPrevious = 0, previous = 1, next = 0;
+            Console.Write("{0} {1}", previousToPrevious, previous);
+            for (int i = 2; i < len; i++)
+            {
+                next = previousToPrevious + previous;
+                Console.Write(" {0}", next);
+                previousToPrevious = previous;
+                previous = next;
+            }
+        }
+    }
+}
--- LogicalProgram/LogicalProgram/Program.cs
+++ LogicalProgram/LogicalProgram/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Logical Programmin");
+
+            //Program-1-FibonacciSeries
+            /*
+               Fibonacci series is a special type of series in which the next term is the sum of the
+               previous two terms. For example, if 0 and 1 are the two previous terms in a series, then
+               the next term will be 1(0+1).
+             */
+
+            FibonacciSeries.Fibonacci_Recurrsion(5);
         }
     }
 }
