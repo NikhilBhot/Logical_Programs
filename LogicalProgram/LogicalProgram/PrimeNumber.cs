df1e8560fb5a3039d23ef737b0687c90b587e1e7
unknown <bhotnikhil25@gmail.com>
2023-02-27 09:53:38 +0530

    [Nikhil]-Prime Number

---
+++ LogicalProgram/LogicalProgram/PrimeNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class PrimeNumber
+    {
+        public static void primeNumber()
+        {
+
+            int number, i, m = 0, flag = 0;
+            Console.Write("Enter the Number to check Prime: ");
+            number = int.Parse(Console.ReadLine());
+            m = number / 2;
+            for (i = 2; i <= m; i++)
+            {
+                if (number % i == 0)
+                {
+                    Console.Write("Number is not Prime.");
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+            {
+                Console.Write("Number is Prime.");
+            }
+        }
+    }
+}
--- LogicalProgram/LogicalProgram/Program.cs
+++ LogicalProgram/LogicalProgram/Program.cs
@@ -25,9 +25,19 @@
                 
                 b. 28 = 1+2+4+7
                 c. 28 = 28
+
+                PerfectNumber.isPerfectNumber();
              */
 
-            PerfectNumber.isPerfectNumber();
+            //Program-3-Prime Number
+            /*
+                Just like the Perfect number, the Prime number is also a special type of number. When
+                the number is divided greater than 1 and divided by 1 or itself is referred to as the Prime
+                number. 0 and 1 are not counted as prime numbers. All the even numbers can be
+                divided by 2, so 2 is the only even prime minister.
+            */
+
+            PrimeNumber.primeNumber();
         }
     }
 }
